1244f57d8d3545353ac88fdc55d625a29858d634
Mac <user@Macs-MacBook-Pro.local>
2022-10-08 12:28:50 +0100

    fix: added fail checks and updated readME

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Array data structures
+# Array data structures in index.js
 Implemented an array data structure in javascript, to include:
 1. get (to access an array index)
 2. push (to add an element to the end of an array)
 3. pop (to remove the last element of an array)
 4. delete (to remove an array element from any index)
 5. insert (to add an element to any index in an array)
+
+# Solve array problems
+1. solved the array problem to reverse a string
+2. 
 
--- reverseAString.js
+++ reverseAString.js
@@ -4,7 +4,10 @@
 
 let string = "Hi my name is paul"
 function reverse1(str) {
-
+    //fail checks
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return 'Provide the correct Input of type string'
+    }
     let reversed = ''
     for (let i = 0; i < string.length; i++) {
         reversed = string[i] + reversed
@@ -15,6 +18,9 @@ function reverse1(str) {
 
 
 function reverse2(str) {
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return 'Provide the correct Input of type string'
+    }
     let returnedArray = []
     for (let i = str.length; i >= 0; i--) {
         returnedArray.push(str[i])
@@ -29,5 +35,5 @@ const reverse3 = (str) => str.split('').reverse().join('')
 
 const reverse4 = (str) => [...str].reverse().join('')
 
-console.log(reverse4(string))
+console.log(reverse1(null))
 
